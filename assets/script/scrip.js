ed22484f8d1eb2390540e092beb203995effd817
Aislan Oliveira de Carvalho <89170451+Aislann@users.noreply.github.com>
2023-12-20 16:55:51 -0300

    Add files via upload

---
+++ assets/script/scrip.js
@@ -0,0 +1,27 @@
+function validarCnpj(cnpj)
+{
+    $.ajax({
+        'url' : 'https://receitaws.com.br/v1/cnpj/' + cnpj.replace(/[^0-9]/g, ''), //.replace(/[^0-9]/g, '') - deixar apenas os numeros do cnpj
+        'type': 'GET',
+        'dataType': 'jsonp',
+        'sucess': function(dado)
+        {
+
+            if(dado.nome == undefined){
+                alert(dado.status + '' + dado.message)
+            } else {
+
+            }            
+            console.log(dado)
+            document.getElementById('razao').value = dado.nome
+            document.getElementById('nomeFantasia').value = dado.fantasia
+            document.getElementById('abertura').value = dado.abertura
+            document.getElementById('status').value = dado.situacao
+            document.getElementById('endereco').value = dado.logradouro
+            document.getElementById('municipio').value = dado.municipio
+            document.getElementById('estado').value = dado.uf
+            document.getElementById('telefone').value = dado.telefone
+
+        }
+    })
+}
