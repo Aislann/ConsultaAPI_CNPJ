59796c9285a9bd2c3a1c0647dc3446bd384941a5
Aislan Oliveira de Carvalho <89170451+Aislann@users.noreply.github.com>
2023-12-21 21:40:48 -0300

    Add files via upload

--- assets/script/scrip.js
+++ assets/script/scrip.js
@@ -1,27 +1,22 @@
-function validarCnpj(cnpj)
-{
+function validarCnpj(cnpj) {
     $.ajax({
-        'url' : 'https://receitaws.com.br/v1/cnpj/' + cnpj.replace(/[^0-9]/g, ''), //.replace(/[^0-9]/g, '') - deixar apenas os numeros do cnpj
-        'type': 'GET',
+        'url':'https://receitaws.com.br/v1/cnpj/' + cnpj.replace(/[^0-9]/g, ''), //.replace(/[^0-9]/g, '') - deixar apenas os numeros do cnpj
+        'type':'GET',
         'dataType': 'jsonp',
-        'sucess': function(dado)
-        {
-
+        'success': function(dado) {
             if(dado.nome == undefined){
                 alert(dado.status + '' + dado.message)
             } else {
-
+                console.log()
+                document.getElementById('razao').value = dado.nome
+                document.getElementById('nomeFantasia').value = dado.fantasia
+                document.getElementById('abertura').value = dado.abertura
+                document.getElementById('status').value = dado.situacao
+                document.getElementById('endereco').value = dado.logradouro
+                document.getElementById('municipio').value = dado.municipio
+                document.getElementById('estado').value = dado.uf
+                document.getElementById('telefone').value = dado.telefone
             }            
-            console.log(dado)
-            document.getElementById('razao').value = dado.nome
-            document.getElementById('nomeFantasia').value = dado.fantasia
-            document.getElementById('abertura').value = dado.abertura
-            document.getElementById('status').value = dado.situacao
-            document.getElementById('endereco').value = dado.logradouro
-            document.getElementById('municipio').value = dado.municipio
-            document.getElementById('estado').value = dado.uf
-            document.getElementById('telefone').value = dado.telefone
-
         }
     })
 }
